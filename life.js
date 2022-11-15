fe88735a017f8dca7f42695130c02d4abb11ef8e
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-11-15 21:47:25 +0600

    complete

--- life.js
+++ life.js
@@ -1,15 +1,17 @@
-const myLife=(aim, workType=0) => {
-    let messages;
-    if (workType == 'hard' || workType == '99%' || workType == '100%' || workType == 'Dolar') {
-        messages = `Congratulations. Your ${aim} is success` 
-    } else {
-        messages = `Oops! Sorry, Not Congratulations. Hard work for ${aim} success`;
-    }
 
-    console.log(messages);
-}
 
-myLife('Love');
-myLife('Job');
-myLife('Perfect Man', '79%');
-myLife('Front End Developer', '99%');
+        const myLife=(aim, workType=0) => {
+            let messages;
+            if (workType == 'hard' || workType == '99%' || workType == '100%') {
+                messages = `Congratulations. Your ${aim} is success`;
+            } else {
+                messages = `Oops! Sorry, Not Congratulations. Hard work for ${aim} success`;
+            }
+
+            console.log(messages);
+        }
+
+        myLife('Love');
+        myLife('Job');
+        myLife('Perfect Man', '79%');
+        myLife('Front End Developer', '99%');
